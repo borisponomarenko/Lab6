956a5e4c615d9442a375350802cb95c320bda49d
borisponomarenko <56125357+borisponomarenko@users.noreply.github.com>
2019-12-14 01:18:49 +0300

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+package lab6;
+
+public class test {
+    public static void main(String[] args) {
+        Car c1 = new Car("kia", "ceed", "md", 600000, 2016);
+        Car c2 = new Car("Nissan", "x-trail", "Cross", 1500000, 2017);
+        Car c3 = new Car("Ford", "Mondeo", "Standart", 230000, 1111);
+        c1.println();
+        c2.println();
+        c3.println();
+    }
+}
